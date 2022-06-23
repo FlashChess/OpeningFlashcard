464a52295c71295e905232a706eceb44a2196181
Amir <amir.maksyutov@outlook.com>
2022-06-23 13:31:27 -0700

    ready

---
+++ src/Redirecting.tsx
@@ -0,0 +1,23 @@
+import { useSearchParams } from "react-router-dom";
+import { setTimeout } from "timers/promises";
+import "./styles.css";
+
+export default function Redirecting() {
+    const LinkPart = "https://flashchess.github.io/flashcard/?";
+
+    const [params, setParams] = useSearchParams();
+    const title = params.get("title");
+    const description = params.get("description");
+    const pgn = params.get("pgn");
+    const move = params.get("move");
+    var turn = params.get("turn");
+    var orientation = params.get("orientation");
+
+    window.location.href = LinkPart + "title=" + title + "&description=" + description + "&pgn=" + pgn + "&move=" + move + "&turn" + turn + "&orientation=" + orientation;
+
+    return (
+        <div className="box">
+            <h1>...Redirecting</h1>
+        </div>
+    );
+}
